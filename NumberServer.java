009fa2af9bb948a26da9de9a07196c2abb8f00b4
Stephanie Lew <sglew@ucsd.edu>
2023-01-18 14:03:45 -0800

    Update NumberServer.java

--- NumberServer.java
+++ NumberServer.java
@@ -8,7 +8,7 @@ class Handler implements URLHandler {
 
     public String handleRequest(URI url) {
         if (url.getPath().equals("/")) {
-            return String.format("Stephanie's number: %d", num);
+            return String.format("Stephanie's number: ", num);
         } else if (url.getPath().equals("/increment")) {
             num += 1;
             return String.format("Number incremented!");
